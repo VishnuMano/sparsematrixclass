590d2ee57b86183bd98f6e82b3b68fe1ff17acf7
Vishnu Mano <vishnu.mano@gmail.com>
2022-09-10 19:27:37 -0400

    add const ref to indices and value vector

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sparsematrixclass
 A sparse matrix class that reduces memory footprint by 10x compared to current sparse matrix classes with associated linear algebra subroutines
+
+Current Development:
+Sparse Run-length Encoding for compressed storage of discrete sparse matrices
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "smc.h"
+
+int main()
+{
+    std::vector<int> sample = {1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 2};
+    smc::VectorCSC test(sample);
+    auto[test_i, test_x] = test.getCSC();
+    for (std::vector<int>::const_iterator iter = test_i.begin(); iter != test_i.end(); iter++)
+    {
+        std::cout << *iter << " ";
+    }
+    std::cout << std::endl;
+    
+    for (std::vector<int>::const_iterator iter = test_x.begin(); iter != test_x.end(); iter++)
+    {
+        std::cout << *iter << " ";
+    }
+    std::cout << std::endl;
+    
+    // test.display();
+}
