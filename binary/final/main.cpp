c5125f123883d54d999a1fc63ae6e2b40c644f9f
Vishnu Mano <vishnu.mano@gmail.com>
2022-12-06 21:05:32 -0500

    restruct

--- binary/final/main.cpp
+++ binary/final/main.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
-
+int countOnes(unsigned int a)
+{
+    unsigned int count = a - ((a >> 1) & 033333333333) - ((a >> 2) & 011111111111);
+    return ((count + (count >> 3)) & 030707070707) % 63;
+}
 
 int main()
 {
-
+    int num = 20758; // 0101000100010110
+    cout << countOnes(num);
 }
