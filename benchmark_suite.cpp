f87e9c7878c4cc8ac2114538ae3398b3f345fef4
Vishnu Mano <vishnu.mano@gmail.com>
2023-01-16 07:08:16 -0500

    stores in times.txt

--- benchmark_suite.cpp
+++ benchmark_suite.cpp
@@ -15,12 +15,6 @@ class Timer
         {
             // Deconstructor calls Stop Function
             Stop();
-            std::ofstream outfile ("times.txt");
-            for (size_t i = 0; i < time_list.size(); ++i)
-            {
-                outfile << time_list[i] << std::endl;
-            }
-            outfile.close();
         }
 
         void Stop()
@@ -31,16 +25,19 @@ class Timer
             long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
             long long stop = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
             // Calculates Duration
-            long long micro_duration = stop - start;
-            time_list.push_back(micro_duration);
+            micro_duration = stop - start;
             double milli_duration = micro_duration * 0.001;
             std::cout << micro_duration << " microseconds\n";
             std::cout << milli_duration << " milliseconds\n";
         }
 
+        long long accessTime()
+        {
+            return micro_duration;
+        }
     private:
         std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
-        std::vector<long long> time_list;
+        long long micro_duration;
 };
 
 void action()
@@ -50,20 +47,21 @@ void action()
         for (size_t j = 0; j < 10; ++j)
         {
             std::cout << "Hello" << std::endl;
-        }
-        
+        }   
     }
-    
 }
 
 int main()
 {
-    for (size_t i = 0; i < 10; ++i)
+    std::ofstream outfile ("times.txt");
+    for (size_t a = 0; a < 101; ++a)
     {
         {
             Timer time;
+            std::cout << "------------- " << a << " --------------" << std::endl;
             action();
+            outfile << time.accessTime() << std::endl;
         }
     }
-    
+    outfile.close();
 }
